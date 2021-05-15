b4494f2e3ee3183166de359c60a4ad05edcca20d
Sarya5746 <84223071+Sarya5746@users.noreply.github.com>
2021-05-15 14:42:35 +0700

    Update veruscoin.sh

--- veruscoin.sh
+++ veruscoin.sh
@@ -1,4 +1,4 @@
-# verus coin
+# veruscoin
 sudo apt get update && apt get upgrade
 wget https://github.com/VerusCoin/nheqminer/releases/download/v0.8.2/nheqminer-Linux-v0.8.2.tgz
 tar -xvzf nheqminer-Linux-v0.8.2.tgz
