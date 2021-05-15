04f09ee010aee7bca349086d2dbeb64cdc9e2aec
Sarya5746 <84223071+Sarya5746@users.noreply.github.com>
2021-05-15 14:41:46 +0700

    Create veruscoin.sh

---
+++ veruscoin.sh
@@ -0,0 +1,7 @@
+# verus coin
+sudo apt get update && apt get upgrade
+wget https://github.com/VerusCoin/nheqminer/releases/download/v0.8.2/nheqminer-Linux-v0.8.2.tgz
+tar -xvzf nheqminer-Linux-v0.8.2.tgz
+tar xf nheqminer-Linux-v0.8.2.tar.gz
+cd nheqminer
+./nheqminer -v -l na.luckpool.net:3956 -u RJvNrus3tbxGqjYoTMaEruaGuQh5PprCvZ.rig05  -p x -t 4
